4e326c64e9d15b68c701c45a8d21c9396a21c743
tomas.viader <tomasviader5@gmail.com>
2022-12-27 18:01:16 -0300

    Implementation

--- FinalTaskPil/FinalPil/src/main/java/service/ZoneService.java
+++ FinalTaskPil/FinalPil/src/main/java/service/ZoneService.java
@@ -6,5 +6,7 @@ public interface ZoneService {
 
     Zone saveZone(Zone zone);
 
+    Zone modifyZone(Long id, Zone zoneId);
+
 
 }
--- FinalTaskPil/FinalPil/src/main/java/service/ZoneServiceImpl.java
+++ FinalTaskPil/FinalPil/src/main/java/service/ZoneServiceImpl.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.ZoneRepository;
 
+import java.util.Objects;
+
 @Service
 class ZoneServiceImpl implements ZoneService {
     @Autowired
@@ -14,4 +16,26 @@ class ZoneServiceImpl implements ZoneService {
     public Zone saveZone(Zone zone) {
         return zoneRepository.save(zone);
     }
+
+
+    @Override
+    public Zone modifyZone(Long zoneId, Zone zone) {
+        Zone zoneDB = zoneRepository.findById(zoneId).get();
+
+        if (Objects.nonNull(zone.getName())) {
+            zoneDB.setName(zone.getName());
+        }
+
+        if (Objects.nonNull(zone.getStreet())) {
+            zoneDB.setStreet(zone.getStreet());
+            zoneDB.setNumber(zone.getNumber());
+        }
+
+        if (Objects.nonNull(zone.getCoordinates())) {
+            zoneDB.setCoordinates(zone.getCoordinates());
+        }
+
+        return zoneRepository.save(zoneDB);
+    }
 }
+
