4b432286f0d399e0ee67b364d4be04e0d3f82056
tomas.viader <tomasviader5@gmail.com>
2023-01-02 20:41:17 -0300

    Repository Test Passed

--- FinalTaskPil/FinalPil/src/test/java/com/example/FinalPil/repository/ZoneRepositoryTest.java
+++ FinalTaskPil/FinalPil/src/test/java/com/example/FinalPil/repository/ZoneRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.FinalPil.repository;
 
 import com.example.FinalPil.model.Zone;
+import com.example.FinalPil.service.ZoneService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,4 +22,22 @@ class ZoneRepositoryTest {
 
         assertNotNull(savedZone);
     }
+
+    @Test
+    void aZoneShouldBeModified(){
+        Zone zone = new Zone(1L, "Almafuerte", "Islas Malvinas", 700, "20");
+        Zone savedZone = zoneRepository.save(zone);
+
+        String newName = "Cordoba";
+        Zone zone1 = new Zone(1L,newName,"Islas Malvinas",700,"20");
+        zone1.setId(1L);
+        zoneRepository.save(zone1);
+
+        Zone updatedZone = zoneRepository.findByName(newName);
+        assertEquals(zone1.getName(),newName);
+        assertEquals(zone1.getCoordinates(),"20");
+
+    }
+
+
 }
